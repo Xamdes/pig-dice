70f42ca2ff389bc488c73e829e945f8725f08377
Steven Colburn <stevencolburn@xamdes.com>
2018-06-13 13:50:57 -0700

    can switch between players and roll die

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,37 @@
+var currentPlayer = new Player("Player One",0);
+var playerOne = new Player("Player One",0);
+var playerTwo = new Player("Player Two",1);
+
 $(function(){
-  $("#turn").text("Player 1");
+  $("#turn").text(currentPlayer.name);
+  $("#btn-roll").click(function(){
+    var temp = (Math.floor(Math.random()*6)+1)
+    $("#dice").text(temp.toString());
+    if(temp === 1)
+    {
+      currentPlayer.tempScore = 0;
+      endTurn();
+    }
+    else
+    {
+      currentPlayer.tempScore += temp;
+    }
+    $("#score").text(currentPlayer.tempScore.toString());
 
+  });
 
+  $("#btn-hold").click(function()
+  {
+    currentPlayer.calculateScore();
+    endTurn();
 
+  });
 });
 
-function Player()
+function Player(playerName,uid)
 {
+  this.name = playerName;
+  this.id = uid;
   this.tempScore = 0;
   this.totalScore = 0;
 }
@@ -14,4 +39,40 @@ function Player()
 Player.prototype.calculateScore = function()
 {
   this.totalScore += this.tempScore;
+  this.tempScore = 0;
+}
+
+function endTurn()
+{
+  if(currentPlayer.id === 0)
+  {
+    playerOne = currentPlayer;
+  }
+  else
+  {
+    playerTwo = currentPlayer;
+  }
+  $("#dice").text("1");
+  $("#score").text("0");
+  startTurn();
+  updateScore();
+}
+
+function startTurn()
+{
+  if(currentPlayer.id === 0)
+  {
+    currentPlayer = playerTwo;
+  }
+  else
+  {
+    currentPlayer = playerOne;
+  }
+  $("#turn").text(currentPlayer.name);
+}
+
+function updateScore()
+{
+  $("#player-one").text(playerOne.totalScore);
+  $("#player-two").text(playerTwo.totalScore);
 }
