6ee7d62500d33e98c68b02e8703a536e90b311bf
Steven Colburn <stevencolburn@xamdes.com>
2018-06-13 11:37:44 -0700

    create layout and tags for output

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
 $(function(){
+  $("#turn").text("Player 1");
+
 
 
 });
+
+function Player()
+{
+  this.tempScore = 0;
+  this.totalScore = 0;
+}
+
+Player.prototype.calculateScore = function()
+{
+  this.totalScore += this.tempScore;
+}
